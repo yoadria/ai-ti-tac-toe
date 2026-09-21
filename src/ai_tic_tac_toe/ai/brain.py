8256272af82e58312a10fd8eca5c72ebdbf9d839
yoadria <tuadria@gmail.com>
2026-09-21 14:55:57 +0000

    data

--- src/ai_tic_tac_toe/ai/brain.py
+++ src/ai_tic_tac_toe/ai/brain.py
@@ -1,3 +1,8 @@
+import json
+from pathlib import Path
+
+BRAIN_PATH = Path(__file__).resolve().parents[3]/"data"/"brain.json"
+
 brain = {}
 
 def get_value(state, action):
@@ -15,3 +20,43 @@ def get_action_values(state, actions):
         value = get_value(state, action)
         values[action] = value
     return values
+
+def load_brain():
+    
+    global brain
+    
+    if not BRAIN_PATH.exists():
+        brain = {}
+        return brain
+    
+    with open(BRAIN_PATH, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        
+    brain = {}
+    
+    for item in data:
+        state = tuple(item["state"])
+        action = item["action"]
+        value = item["value"]
+        
+        brain[(state, action)] = value
+        
+    return brain
+
+def save_brain():
+    
+    BRAIN_PATH.parent.mkdir(parents=True, exist_ok=True)
+    
+    data = []
+    
+    for (state, action), value in brain.items():
+        data.append(
+            {
+                "state": list(state),
+                "action": action,
+                "value": value,
+            }
+        )
+        
+    with open(BRAIN_PATH, "w", encoding="utf-8") as file:
+        json.dump(data, file, intent=4)
